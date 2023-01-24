0f358abed4f4442651be28193610cf7fdd9eb654
SaraDonzellini <saradonzie@gmail.com>
2023-01-24 17:05:58 +0100

    added html to request.php, added a new variable

--- request.php
+++ request.php
@@ -1,27 +1,42 @@
-<section>
-  <p>
-    <?php
-    echo $_POST['paragrafo'];
-    ?>
-  </p>
-  <h5>
-    <?php
-    echo strlen($_POST['paragrafo']);
-    ?>
-  </h5>
-</section>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Badwords Request</title>
+</head>
+<body>
+  <main>
+    <section>
+      <p>
+        <?php
+        $paragraph = $_POST['paragrafo'];
+        echo $paragraph;
+        ?>
+      </p>
+      <h5>
+        <?php
+        echo strlen($paragraph);
+        ?>
+      </h5>
+    </section>
+    
+    <section>
+      <p>
+        <?php
+    
+        $replacedString = str_ireplace($_POST['censura'], '***', $paragraph);
+        echo $replacedString;
+        ?>
+      </p>
+      <h5>
+        <?php
+        echo strlen($replacedString);
+        ?>
+      </h5>
+    </section>
 
-<section>
-  <p>
-    <?php
-
-    $replacedString = str_ireplace($_POST['censura'], '***', $_POST['paragrafo']);
-    echo $replacedString;
-    ?>
-  </p>
-  <h5>
-    <?php
-    echo strlen($replacedString);
-    ?>
-  </h5>
-</section>
+  </main>
+</body>
+</html>
