03ed46d93cc7a90b39e1ec147dd20aa266d7ba42
SaraDonzellini <saradonzie@gmail.com>
2023-01-24 13:23:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <main>
+    <section>
+      <input type="text" name="paragrafo" id="paragrafo" placeholder="inserisci un paragrafo">
+      <input type="text" name="censura" id="censura" placeholder="inserisci la parola da censurare">
+      <button type="submit">Invia</button>
+    </section>
+  </main>
+</body>
+</html>
